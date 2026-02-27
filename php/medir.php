47a103e1c2ba929b656f9775b44e4ba30fd46a0b
Rodrigo <rodrigomendezrosas@gmail.com>
2026-02-26 21:07:20 -0600

    Use absolute python3 path for sudo measurement command

--- php/medir.php
+++ php/medir.php
@@ -10,14 +10,19 @@ if (!file_exists($scriptPath)) {
 	exit;
 }
 
-$command = 'python3 ' . escapeshellarg($scriptPath) . ' 2>&1';
+$command = 'sudo -n /usr/bin/python3 ' . escapeshellarg($scriptPath) . ' 2>&1';
 $output = [];
 $exitCode = 0;
 exec($command, $output, $exitCode);
 
 if ($exitCode !== 0) {
 	http_response_code(500);
-	echo 'Error al medir: ' . implode(' | ', $output);
+	$error = implode(' | ', $output);
+	if (stripos($error, 'a password is required') !== false || stripos($error, 'sudo:') !== false) {
+		echo 'Error al medir: falta permiso sudo para www-data. Configura sudoers para python3 en este script.';
+	} else {
+		echo 'Error al medir: ' . $error;
+	}
 	exit;
 }
 
