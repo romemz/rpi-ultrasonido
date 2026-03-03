4fc9991863d0cd167c03cb391dd190490e682227
Rodrigo <rodrigomendezrosas@gmail.com>
2026-03-03 13:34:37 -0600

    docs: limpia encabezado de ultrasonido.py

--- python/ultrasonido.py
+++ python/ultrasonido.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 # pyright: reportMissingImports=false
 """
-Tomado en gran parte de:
-https://electrosome.com/hc-sr04-ultrasonic-sensor-raspberry-pi/
+
 
 Formula para calcular la distancia
 
